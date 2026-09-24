4f5d1cfe504c6c3cef6517ca4eb5287506c407aa
ORiONx888 <pxmurphy@gmail.com>
2026-09-24 18:40:49 -0300

    Phase 1: export JevSentinel core

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export * from "./schema.js";
+export * from "./intelligence.js";
+export * from "./state.js";
+export * from "./temporal.js";
+export * from "./jev.js";
+export * from "./telemetry.js";
+export * from "./engine.js";
