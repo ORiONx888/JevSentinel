46e5b3af7eefbadc48b6803f9c60a516e2cdc9fb
ORiONx888 <pxmurphy@gmail.com>
2026-09-25 09:50:29 -0300

    Export native research providers for tests and consumers

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ export { buildEvidenceState } from "./evidence.js";
 export { IntelligenceProvider } from "./intelligence.js";
 export { buildTemporalState } from "./temporal.js";
 export { MemoryTelemetry } from "./telemetry.js";
+export { createNativeRiskProvider, createTransferFlowProvider, createTokenResearchProvider } from "./providers/index.js";
